e87d5090de3815a888c3bcfa224221167d5581af
Sphexator <kristoffer.tunlid@gmail.com>
2018-11-05 11:09:42 +0100

    simple start

---
+++ Config/Config.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+const config = require('./config.json');
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
+const finalConfig = _.merge(defaultConfig, environmentConfig);
+
+global.gConfig = finalConfig;
+
+console.log(`Config file loaded`);
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Beginning of file
+const config = require('./config/config.js');
+
+var bot = new Eris(global.gConfig.token);
+bot.on("ready", () => {
+    console.log("Ready!");
+});
+bot.on("messageCreate", (msg) => {
+    if(msg.content === "!ping") {
+        bot.createMessage(msg.channel.id, "Pong!");
+    }
+});
+bot.connect();
